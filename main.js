489d19d28b41b30ba0106b0ef2ee694d432a9c6f
Abdelilah EL Haddad <elhaddadabdelilah@gmail.com>
2021-08-18 19:31:57 +0100

    :iphone: :seedling: Mobile Responsive :sparkles: :iphone:

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 import "tailwindcss/tailwind.css"
+
+const NavBtn = document.querySelector('#NavBtn');
+const NavClose = document.querySelector('#NavClose');
+const NavBar = document.querySelector('#NavBar');
+
+NavBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  NavBar.classList.add('sm:flex');
+  NavBar.classList.remove('sm:hidden');
+});
+
+NavClose.addEventListener('click', (e) => {
+  e.preventDefault();
+  NavBar.classList.add('sm:hidden');
+  NavBar.classList.remove('sm:flex');
+});
