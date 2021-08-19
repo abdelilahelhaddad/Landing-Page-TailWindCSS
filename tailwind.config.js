a58c3524702c92145c25ede2ba4b7f7e3918a1c2
Abdelilah EL Haddad <elhaddadabdelilah@gmail.com>
2021-08-19 15:56:31 +0100

    :iphone: :seedling: Mobile Responsive :sparkles: :iphone:

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,20 +10,30 @@ module.exports = {
       }
     },
     screens: {
-      '2xl': {'max': '1535px'},
-      // => @media (max-width: 1535px) { ... }
+      '2xl': {
+        'max': '1536px'
+      },
+      // => @media (max-width: 1536px) { ... }
 
-      'xl': {'max': '1279px'},
-      // => @media (max-width: 1279px) { ... }
+      'xl': {
+        'max': '1280px'
+      },
+      // => @media (max-width: 1280px) { ... }
 
-      'lg': {'max': '1023px'},
-      // => @media (max-width: 1023px) { ... }
+      'lg': {
+        'max': '1024px'
+      },
+      // => @media (max-width: 1024px) { ... }
 
-      'md': {'max': '767px'},
-      // => @media (max-width: 767px) { ... }
+      'md': {
+        'max': '768px'
+      },
+      // => @media (max-width: 768px) { ... }
 
-      'sm': {'max': '639px'},
-      // => @media (max-width: 639px) { ... }
+      'sm': {
+        'max': '640px'
+      },
+      // => @media (max-width: 640px) { ... }
     }
   },
   variants: {
