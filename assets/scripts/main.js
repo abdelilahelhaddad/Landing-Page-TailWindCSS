c2f36edda89193b75dd4c98467c13a56f051097d
Abdelilah EL Haddad <elhaddadabdelilah@gmail.com>
2021-08-26 20:59:36 +0100

    :fire: :art: Commit Desktop Layout :sparkles: :globe_with_meridians:

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -15,3 +15,22 @@ NavClose.addEventListener('click', (e) => {
   NavBar.classList.add('md:hidden');
   NavBar.classList.remove('md:flex');
 });
+
+// Initialize and add the map
+function initMap() {
+  // Your location
+  const loc = {
+    lat: 42.361145,
+    lng: -71.057083
+  };
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 14,
+    center: loc
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({
+    position: loc,
+    map: map
+  });
+}
